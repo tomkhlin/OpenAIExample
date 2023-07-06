fc0e286f8f5f2121b0ab1aeb735a97ee581bacbc
林廣學 <tomkhlin@g.chu.edu.tw>
2023-07-06 15:17:02 +0800

    加入專案檔案。

---
+++ ChatGPTAPIExample/OpenAIRequestBody.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ChatGPTAPIExample
+{
+    public class OpenAIRequestBody
+    {
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
+
+        [JsonPropertyName("messages")]
+        public List<Message> Messages { get; set; }
+    }
+}
---
+++ ChatGPTAPIExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net;
+
+namespace ChatGPTAPIExample
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //API endpoint URL and key
+            const string endpoint = "你的API URL";
+            const string key = "你的Key";
+
+            //Create a new HttpClient and set the authorization header
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
+
+            //Create a new message and add it to the list of messages
+            var message = new Message
+            {
+                Role = "user",
+                Content = "Hello, I'm a user."
+            };
+            var content = new OpenAIRequestBody
+            {
+                Model = "gpt-3.5-turbo",
+                Messages = new List<Message>()
+            };
+            content.Messages.Add(message);
+
+            //Serialize the content to JSON and send it to the API
+            string json_string = JsonSerializer.Serialize(content);
+            using StringContent jsonContent = new StringContent(json_string, Encoding.UTF8, "application/json");
+            using HttpResponseMessage response = await client.PostAsync(endpoint, jsonContent);
+
+            //If the response is OK, print the response body
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            Console.ReadKey();
+        }
+    }
+}
